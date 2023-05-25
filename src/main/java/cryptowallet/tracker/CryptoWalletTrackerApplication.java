382b38e5397d9e3d2e0a8c16bae05f770035e76f
William Poletto <poletto123@gmail.com>
2023-05-25 13:54:05 -0300

    Adding base spring boot project

---
+++ src/main/java/cryptowallet/tracker/CryptoWalletTrackerApplication.java
@@ -0,0 +1,13 @@
+package cryptowallet.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWalletTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoWalletTrackerApplication.class, args);
+	}
+
+}

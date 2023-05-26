b6fcabaaa9128d25dbf88e29c62a272a978edfd2
William Poletto <poletto123@gmail.com>
2023-05-26 18:10:33 -0300

    Adding base unit tests

--- src/test/java/cryptowallet/tracker/CryptoWalletTrackerApplicationTests.java
+++ src/test/java/cryptowallet/tracker/CryptoWalletTrackerApplicationTests.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
 @SpringBootTest
 class CryptoWalletTrackerApplicationTests {
 
@@ -13,12 +18,27 @@ class CryptoWalletTrackerApplicationTests {
 
 	@Test
 	void getBitcoinCurrentPrice() {
-		System.out.println(cryptoPriceService.getCurrentPrice("BTC", null, null));
+		String tokenSymbol = "BTC";
+
+		BigDecimal price = cryptoPriceService.getCurrentPrice(tokenSymbol, null, null)
+				.getPriceInformationSymbol()
+				.getPriceUsd();
+
+		assertEquals(price.scale(), 16, "Incorrect scale on price");
+		assertEquals(price.precision(), 21, "Incorrect precision on price");
 	}
 
 	@Test
 	void getBitcoinPriceAtTimestamp() {
-		System.out.println(cryptoPriceService.getPriceAtTimestamp("BTC", "1617753600000", "1617753601000"));
+		String tokenSymbol = "BTC";
+		String startTimestamp = "1617753600000";
+		String endTimestamp = "1617753601000";
+
+		BigDecimal price = cryptoPriceService.getPriceAtTimestamp(tokenSymbol, startTimestamp, endTimestamp)
+				.getPriceInformationTimestamp().get(0)
+				.getPriceUsd();
+
+		assertEquals(price, new BigDecimal("56999.9728252053067291"), "Incorrect timestamp price");
 	}
 
 }
